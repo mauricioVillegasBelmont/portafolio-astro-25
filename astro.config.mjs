23ac791cbbcc9018b59cf00f77fe20956218f7d6
miau <mauricio@pymeweb.mx>
2026-03-04 02:25:36 -0600

    integrated gtag for analytics logic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mauriciovillegasbelmont.github.io',
-  base:'/portafolio-astro-25',
-  integrations: [
-    tailwind(),
-    icon(),
-  ],
-  vite: {
-    resolve: {
-      alias: {
-        components: '/src/components',
-        layouts: '/src/layouts',
-        modules: '/src/modules',
-        pages: '/src/pages',
-        styles: '/src/styles',
-        assets: '/src/assets',
-        data: '/src/data',
-        icons: '/src/icons',
-        libs: '/src/libs',
-        utils: '/src/utils',
-      }
-    }
-  }
+	site: "https://mauriciovillegasbelmont.github.io",
+	base: "/portafolio-astro-25",
+	integrations: [
+		tailwind(),
+		icon(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"], // Reenvía las llamadas a dataLayer
+			},
+		}),
+	],
+	vite: {
+		resolve: {
+			alias: {
+				components: "/src/components",
+				layouts: "/src/layouts",
+				modules: "/src/modules",
+				pages: "/src/pages",
+				styles: "/src/styles",
+				assets: "/src/assets",
+				data: "/src/data",
+				icons: "/src/icons",
+				libs: "/src/libs",
+				utils: "/src/utils",
+			},
+		},
+	},
 });
